01a337b5f8c9bd35f6f62d4b07d4c80510065621
Gunabh Sharan <gunabhsharan.25@gmail.com>
2025-07-11 22:35:56 +0530

    commit

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -1,6 +1,6 @@
 import jwt from 'jsonwebtoken';
 
-const JWT_SECRET = process.env.JWT_SECRET || '7f5a66c5673e5f96d670355fc6d301c9664d18a90c7c8d2dfa0e3d693c1e60d80dcac353c6fd5746cec6b8c5674601b83042a260ece97f2ebe5596be1b809a13';
+const JWT_SECRET = process.env.JWT_SECRET || 'your-secret-key';
 
 export function verifyToken(token) {
   try {
